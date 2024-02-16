66a789612fde4b4555583c901468f2893c2d80ef
corey <cjnj08@gmail.com>
2024-02-15 21:57:41 -0500

    uploading changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/navbar';
-import Footer from './components/Footer'; // Adjust the import path based on your project structure
-import QuantityPicker from './components/quantitypicker';
-import Catalog from './components/pages/catalog';
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Footer from "./components/Footer"; // Adjust the import path based on your project structure
+import QuantityPicker from "./components/quantitypicker";
+import Home from "./pages/home";
+import Catalog from "./pages/catalog";
+import About from "./pages/about";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <Navbar></Navbar>
+      <Home></Home>
       <Catalog></Catalog>
+      <About></About>
       <Footer></Footer>
-      
     </div>
   );
 }
 
 export default App;
-
-
---
+++ src/pages/catalog.jsx
@@ -0,0 +1,34 @@
+import "./catalog.css";
+import Product from "../components/products";
+import { useEffect, useState } from "react";
+import DataService from "../services/dataservice";
+
+function Catalog() {
+  const [productsState, setProducts] = useState([]);
+  const [status, setStatus] = useState("blank");
+
+  useEffect(function () {
+    console.log("component is loaded");
+    loadCatalog();
+  }, []);
+
+  function loadCatalog() {
+    let service = new DataService();
+    let products = service.getProducts();
+    setProducts(products);
+    setStatus("Ready");
+    console.log(products);
+  }
+
+  return (
+    <div className="catalog">
+      <h1>Look at my awsome{productsState.length} products!</h1>
+      <h5>Page status: {status}</h5>
+      {productsState.map((productArrow) => (
+        <Product data={productArrow}></Product>
+      ))}
+    </div>
+  );
+}
+
+export default Catalog;
